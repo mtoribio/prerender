fce148573060ee70dc9f72520b8428be7cfe4a40
farmy00 <marcos@toribioespadas.es>
2018-07-18 17:29:18 +0200

    update options server

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
+const memoryCache = require('cache');
 
-var server = prerender();
+var server = prerender({
+    port: 5000,
+    logRequests: true,
+    pageLoadTimeout: 1000000,
+    waitAfterLastRequest: 250,
+    chromeFlags: [
+        '--headless',
+        '--no-sandbox',
+        '--disable-gpu',
+        '--remote-debugging-port=9222',
+        // '--hide-scrollbars'
+    ]
+});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+
+server.use(memoryCache);
 
 server.start();
