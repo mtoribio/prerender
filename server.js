1dcdeb17aac23c553fb23f39492dfe5c23b8a1e6
mtoribio <marcos.toribio.farmy@gmail.com>
2018-09-05 15:24:13 +0200

    Remove logger, add incognito, waitforprerender for all request

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
-const memoryCache = require('cache');
 
-var server = prerender({
-    port: 5000,
-    logRequests: true,
-    pageLoadTimeout: 1000000,
-    waitAfterLastRequest: 250,
-    chromeFlags: [
-        '--headless',
-        '--no-sandbox',
-        '--disable-gpu',
-        '--remote-debugging-port=9222',
-        // '--hide-scrollbars'
-    ]
+const server = prerender({
+  port: 5000,
+  // logRequests: true,
+  pageLoadTimeout: 1000 * 60 * 5,
+  waitAfterLastRequest: 250,
+  pageDoneCheckInterval: 1000 * 30,
+  // chromeLocation: '/usr/bin/chromium-browser',
+  chromeFlags: [
+    '--headless',
+    '--disable-gpu',
+    '--remote-debugging-port=9222',
+    '--incognito',
+    // '--auto-open-devtools-for-tabs'
+    // '--hide-scrollbars'@@
+  ]
 });
 
 server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.blockResources());
+server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-
-server.use(memoryCache);
 
 server.start();
