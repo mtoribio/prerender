5c5b50fea0508eefc4721f1e2a4d4820c964b931
mtoribio <marcos.toribio.farmy@gmail.com>
2018-09-10 11:22:13 +0200

    Add response validation and chrome flags.

--- server.js
+++ server.js
@@ -2,20 +2,21 @@
 var prerender = require('./lib');
 
 const server = prerender({
-  port: 5000,
-  // logRequests: true,
-  pageLoadTimeout: 1000 * 60 * 5,
-  waitAfterLastRequest: 250,
-  pageDoneCheckInterval: 1000 * 30,
-  chromeLocation: '/usr/bin/chromium-browser',
-  chromeFlags: [
-    '--headless',
-    '--disable-gpu',
-    '--remote-debugging-port=9222',
-    '--incognito',
-    // '--auto-open-devtools-for-tabs'
-    // '--hide-scrollbars'@@
-  ]
+    port: 5000,
+    // logRequests: true,
+    pageLoadTimeout: 1000 * 60 * 5,
+    waitAfterLastRequest: 250,
+    pageDoneCheckInterval: 1000 * 30,
+    chromeLocation: '/usr/bin/chromium-browser',
+    chromeFlags: [
+        '--headless',
+        '--disable-gpu',
+        '--remote-debugging-port=9222',
+        '--incognito',
+        '--silent-launch'
+        // '--auto-open-devtools-for-tabs'
+        // '--hide-scrollbars'@@
+    ]
 });
 
 server.use(prerender.sendPrerenderHeader());
