15159c508d26e5a4c938fa35ce999c4da5b2b0a8
Denis Kolesnichenko <denis@farmy.ch>
2018-09-29 18:38:51 +0200

    Automatic platform detection to switch Chrome path for MacOS

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
+const os = require('os');
+
+var chromeLocation = '/usr/bin/chromium-browser';
+
+if (os.platform() == 'darwin')
+    chromeLocation = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'; // for local MacOs standard Chrome
 
 const server = prerender({
     port: 5000,
     // logRequests: true,
     pageLoadTimeout: 1000 * 60 * 5,
     waitAfterLastRequest: 250,
-    pageDoneCheckInterval: 1000 * 30,
-    chromeLocation: '/usr/bin/chromium-browser',
+    pageDoneCheckInterval: 500,
+    chromeLocation: chromeLocation,
     chromeFlags: [
         '--headless',
         '--disable-gpu',
         '--remote-debugging-port=9222',
         '--incognito',
-        '--silent-launch'
+        '--silent-launch',
+        '--window-size=1024,768'
         // '--auto-open-devtools-for-tabs'
         // '--hide-scrollbars'@@
     ]
